a8a3b7779017a85d99614c1692a74642c44d7f34
Kiril Kobzar <kirilkobzar@gmail.com>
2022-12-29 16:10:42 +0200

    layout title fix

--- my-project/resources/js/Pages/Chat/Container.vue
+++ my-project/resources/js/Pages/Chat/Container.vue
@@ -1,5 +1,5 @@
 <template>
-    <AppLayout title="Dashboard">
+    <AppLayout title="Chat">
         <template #header>
             <h2 class="font-semibold text-xl text-gray-800 leading-tight">
                 <ChatRoomSelection
